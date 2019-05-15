3b0e461ffad3d82ab267c9806a0141ef23cdf867
孟辰 <sisimengchen@gmail.com>
2019-05-15 18:45:52 +0800

    重命名

---
+++ src/components/Units/TitleUnit/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Form } from 'antd';
+import PropComponet from '../common/PropComponet';
+import { formItemLayout } from '../common';
+
+const TitleUnit = props => (
+  <Form className="components-unit components-unit-meta" {...formItemLayout}>
+    <PropComponet key="text" propName="text" {...props} />
+    <PropComponet key="href" propName="href" {...props} />
+    <PropComponet key="fontSize" propName="fontSize" {...props} />
+    <PropComponet key="color" propName="color" {...props} />
+    <PropComponet key="textAlign" propName="textAlign" {...props} />
+    <PropComponet key="margin" propName="margin" {...props} />
+    <PropComponet key="padding" propName="padding" {...props} />
+  </Form>
+);
+
+export default TitleUnit;
